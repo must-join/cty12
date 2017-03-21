a13ae852d0ab8b581dc38f00c55c72f535596e22
henrytrueheart <henrychou0719@gmail.com>
2017-03-21 14:39:16 +0800

    first upload
    3/21 upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(evt){
+  if ($(window).scrollTop()>0)     
+
+
+$(".navbar").removeClass("navbar-top")
+  
+
+  else
+   
+  $(".navbar").addClass("navbar-top")
+
+});
+
+
+
+
+
